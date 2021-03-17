67bb42ef9a8282352c3af93ecfaf05c34bf436fe
Rafi0321 <Rafi.sadiq21@gmail.com>
2021-03-16 21:36:41 -0400

    My Java Commit

---
+++ src/gitClassTwoGithub/NewclassGitStaged.java
@@ -0,0 +1,13 @@
+package gitClassTwoGithub;
+
+public class NewclassGitStaged {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			We should all appreciate git
+			we use github and we push this code
+			to github and use come magical commond
+			to perform the task
+	}
+
+}
