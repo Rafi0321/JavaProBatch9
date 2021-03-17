e1cc872d29e8ca23d6e184319a5007e6f458f654
Rafi0321 <Rafi.sadiq21@gmail.com>
2021-03-16 20:53:26 -0400

    My Review commit

--- src/gitClassTwoGithub/SecondCommmit.java
+++ src/gitClassTwoGithub/SecondCommmit.java
@@ -4,6 +4,9 @@ public class SecondCommmit {
  public static void main(String [] args) {
 	 writing code is fun and always will be 
 	 hurra!!!!!!!!!!!!!!
+	 
+	 System.out.println("Why coding is good to do")
+	 System.out.println("It is new code for git and we change it")
  }
 
 }
